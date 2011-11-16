f4680c94fa7568956efbac10c7a94c366a44c8bf
Marc <mphoeppner@gmail.com>
2011-11-16 10:57:01 +0100

    include transcript data infrastructure

--- lib/files/activerecord.rb
+++ lib/files/activerecord.rb
@@ -5,6 +5,7 @@ module ExpressionDB
 	
 	class Dataset < DBConnection
 		set_primary_key 'dataset_id'
+		has_many :samples
 	end
 	
 	class Sample < DBConnection
@@ -26,4 +27,17 @@ module ExpressionDB
 		has_many :samples, :through => :xref_gene_samples
 	end
 
+	class Transcript < DBConnection
+		set_primary_key 'transcript_id'
+		has_many :xref_transcript_samples
+		has_many :samples, :through => :xref_transcript_samples
+		belongs_to :gene
+	end
+	
+	class XrefTranscriptSample < DBConnection
+		set_primary_keys :transcript_id,:sample_id
+		belongs_to :transcript, :foreign_key => "transcript_id"
+		belongs_to :sample, :foreign_key => "sample_id"
+	end
+	
 end
